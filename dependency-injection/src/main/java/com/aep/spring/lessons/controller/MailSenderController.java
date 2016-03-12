16a7876893a929b56606ad0dfd269e7f4ce43af1
unknown <emrah.pekesen@emrahpekesen-nb.ISTDOM.com>
2016-03-12 19:06:15 +0200

    @Value Annotation Implementation

--- dependency-injection/src/main/java/com/aep/spring/lessons/controller/MailSenderController.java
+++ dependency-injection/src/main/java/com/aep/spring/lessons/controller/MailSenderController.java
@@ -2,6 +2,7 @@ package com.aep.spring.lessons.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +12,20 @@ import com.aep.spring.lessons.service.MailSender;
 @RestController
 public class MailSenderController {
 	private Mail mail;
+	
+	@Value("${mail.to}")
+	private String to;
+	@Value("${mail.subject}")
+	private String subject;
+	@Value("${mail.body}")
+	private String body;
+	
 
 	@Autowired
 	public void setMail(Mail mail) {
 		this.mail = mail;
-		this.mail.setTo("emrahpekesen@gmail.com").setSubject("Reqource Annotation Kullanımı")
-				.setBody("Bu Örnek Resource Annotation ını kullanarak Dependency Injecttion Yapmayı Anlatıyor..");
+		this.mail.setTo(to).setSubject(subject)
+				.setBody(body);
 	}
 
 	@Autowired
